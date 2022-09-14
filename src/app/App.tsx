bda9329d852697448a50774390715c732dea7bfa
Ihor Dzvonyk <fabionfox7@gmail.com>
2022-09-14 09:19:16 +0300

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,31 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Redirect from './pages/Redirect';
+import Revenue from './pages/Revenue';
+import RevenueList from './pages/RevenueList';
+
+// import { Helmet } from "react-helmet";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    {/* <Helmet>
+      <title>GIC Canada</title>
+      <meta name="description" content="GIC Canada Immigration Company" />
+    </Helmet> */}
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center">Loading...</div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Redirect />} />
+        <Route path="/list" element={<RevenueList />} />
+        <Route path="/item/:id" element={<Revenue />} />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+  );
+}
+
+export default App;
