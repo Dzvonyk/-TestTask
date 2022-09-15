ae2f0e8527a3ea2a8333b71583015d5e60a29bcb
Ihor Dzvonyk <fabionfox7@gmail.com>
2022-09-15 16:22:52 +0300

    revenue list UI

--- src/app/pages/RevenueList/RevenueList.tsx
+++ src/app/pages/RevenueList/RevenueList.tsx
@@ -2,23 +2,45 @@ import React, { useEffect } from "react";
 import RevenueCard from "./components/RevenueCard";
 import useRevenueList from "./hooks/useRevenueList";
 
+import { ReactComponent as IconSearch } from "./../../../assets/search.svg";
+
+
 const RevenueList = () => {
-  const { getRevenueListHandler , revenueList } = useRevenueList();
+  const { getRevenueListHandler, revenueList } = useRevenueList();
 
   useEffect(() => {
     getRevenueListHandler();
     console.log("-| getRevenueListHandler  useEffect |-", revenueList);
   }, []);
-  
+
   return (
     <div className="px-[55px] pt-[115px]">
-      <div>input</div>
-      <div className="flex flex-col gap-2">
-      {revenueList &&
-        revenueList.length > 0 &&
-        revenueList.map((revenue) => (
-          <RevenueCard key={revenue.id} revenue={revenue} />
-        ))}
+      <label className="relative block">
+        <span className="sr-only">Search</span>
+        <span className="absolute inset-y-0 left-0 flex items-center pl-[18px]">
+          <IconSearch />
+        </span>
+        <input
+          className="text-xs placeholder:text-[#6B7280] placeholder:opacity-50 block bg-[#F9FAFB] w-80 border border-[#E5E7EB] rounded-lg py-3 pl-11 pr-3 focus:outline-none focus:ring-0"
+          placeholder="Search"
+          type="text"
+          name="search"
+        />
+      </label>
+
+      <div className="flex flex-col border border-[#E5E7EB] rounded-lg overflow-hidden mt-[50px]">
+        <div className="flex flex-row bg-[#F9FAFB] uppercase text-xs text-[#6B7280] font-medium justify-between">
+          <div className="flex flex-row w-1/2">
+            <div className="px-6 py-3 w-1/2 cursor-pointer">Name</div>
+            <div className="px-6 py-3 w-1/2 cursor-pointer">Date</div>
+          </div>
+          <div className="px-6 py-3 w-2/12 cursor-pointer">state</div>
+        </div>
+        {revenueList &&
+          revenueList.length > 0 &&
+          revenueList.map((revenue) => (
+            <RevenueCard key={revenue.id} revenue={revenue} />
+          ))}
       </div>
     </div>
   );
--- src/app/pages/RevenueList/components/RevenueCard/RevenueCard.tsx
+++ src/app/pages/RevenueList/components/RevenueCard/RevenueCard.tsx
@@ -2,17 +2,34 @@
 import React, { FunctionComponent } from "react";
 import { NavLink } from "react-router-dom";
 import { GetRevenueListResponseType } from "../../../../../types/RevenueType";
+import dayjs from "dayjs";
 
 type RevenueCardProps = {
-    revenue: GetRevenueListResponseType;
-  };
+  revenue: GetRevenueListResponseType;
+};
 
 const RevenueCard: FunctionComponent<RevenueCardProps> = ({ revenue }) => {
+  const { name, date, isActive } = revenue;
+  const Date = dayjs(date).format("DD.MM.YYYY");
+
   return (
-    <NavLink
-      to={`/item/${revenue.id}`}
-    >
-      {`${revenue.name} -|- ${revenue.date} -|- ${revenue.isActive}`}
+    <NavLink to={`/item/${revenue.id}`}>
+      <hr className=" w-full bg-[#E5E7EB]" />
+      <div className="flex flex-row hover:bg-[#F6F6F6] text-sm justify-between">
+        <div className="flex flex-row w-1/2">
+          <div className="px-6 py-4 text-[#111827] font-medium w-1/2">
+            {name}
+          </div>
+          <div className="px-6 py-4 text-[#6B7280] w-1/2">{Date}</div>
+        </div>
+        <div className="px-6 py-4 w-2/12">
+          {isActive ? (
+            <div className="text-[#5D5FEF]">Active</div>
+          ) : (
+            <div className="text-[#EF5DA8] ">Disable</div>
+          )}
+        </div>
+      </div>
     </NavLink>
   );
 };
